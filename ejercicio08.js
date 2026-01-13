0da948f803b4e59e58b884e212945b913162dab3
eric08solanovalencia-cloud <eric08solanovalencia@gmail.com>
2026-01-13 16:18:24 -0600

    Update ejercicio08.js

--- ejercicio08.js
+++ ejercicio08.js
@@ -5,8 +5,9 @@
 // concatenarCadenas("Hola", "Mundo") => "HolaMundo"
 // concatenarCadenas("Frase", "de prueba") => "Frasede prueba"
 
-//escribe aquí tu función. Recuerda que el nombre de la función debe ser exactamente "concatenarCadenas"
-
+function concatenarCadenas(cadena1, cadena2) {
+    return cadena1 + cadena2; // Retorna la concatenación de ambas cadenas
+}
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     concatenarCadenas
