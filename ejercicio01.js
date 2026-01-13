7f68c8d63cbbd96f3a793e1b18ef4cfd0fbf410e
eric08solanovalencia-cloud <eric08solanovalencia@gmail.com>
2026-01-13 16:09:52 -0600

    Update ejercicio01.js

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,7 +2,10 @@
 // Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    // Creamos la variable nombre y le asignamos "Eric"
+    const nombre = "Eric";
+    // Retornamos la variable
+    return nombre;
 }
 
 // No modificar.
